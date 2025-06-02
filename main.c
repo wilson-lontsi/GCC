d998a730e45bf977ecb8e963e5516c7414f75d5f
wilson-lontsi <lontsiwilson40@gmail.com>
2025-06-02 14:38:00 +0000

    arrays

---
+++ main.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+
+int main() {
+          int average;
+            int grates[5];
+              grades[0] = 80;
+                grades[2] = 90;
+                  average = (grades[0] + grades[1] + grades[2]) / 3;
+                    printf("The average of the 3 grades is: %d", average);
+
+                      return 0;
+}
+
